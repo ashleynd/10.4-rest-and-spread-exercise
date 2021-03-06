3ba2d8009fd8a1875662ae360f8769795356d9ae
Ashley Dragan <ashleydragan@Ashleys-MacBook-Air.local>
2021-03-05 19:05:16 -0500

    adding all the files

---
+++ app.js
@@ -0,0 +1,20 @@
+const shoppingCart = [
+	{
+		name     : 'honey orchid',
+		quantity : 2,
+		price    : 13.5
+	},
+	{
+		name     : 'african solstice',
+		quantity : 4,
+		price    : 25.99
+	}
+];
+
+const cartCopy = [ ...shoppingCart ];
+
+
+// function sum(...nums){
+// 	return nums.reduce((sum,n) => sum + n);
+// }
+
