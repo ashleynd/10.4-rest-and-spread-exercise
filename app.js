4f33ce095a7f21bdcfc4790d1f2871a3c9c1d9ee
Ashley Dragan <ashleydragan@Ashleys-MacBook-Air.local>
2021-03-05 20:44:04 -0500

    added in attempts and solutions

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-const shoppingCart = [
-	{
-		name     : 'honey orchid',
-		quantity : 2,
-		price    : 13.5
-	},
-	{
-		name     : 'african solstice',
-		quantity : 4,
-		price    : 25.99
-	}
-];
-
-const cartCopy = [ ...shoppingCart ];
 
 
-// function sum(...nums){
-// 	return nums.reduce((sum,n) => sum + n);
-// }
+// ES2015 Function that takes a variable number of arguments
+const filterOutOdds = (...args) => args.filter(v => v % 2 === 0)
 
+
+// findMin 
+const min = function() {
+	const args = Array.from(arguments);
+	return args.reduce((min, currVal) => {
+		return currVal < min ? currVal : min;
+	});
+};
+
+function findMin(...min){
+	return min.reduce((min, n) => min);
+}
+
+// mergeObjects
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
+
+
+// doubleAndReturnArgs
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v *2)];
+
+// SLICE AND DICE 
+const removeRandom = items => {
+	let idx = Math.floor(Math.random() * items.length);
+	return [...items.slice(0, idx), ...items.slice(idx + 1)];
+  }
+
+
+  const extend = (array1, array2) => {
+	return [...array1, ...array2];
+  }
+
+
+  const addKeyVal = (obj, key, val) => {
+  let newObj = { ...obj }
+  newObj[key] = val;
+  return newObj;
+  }
+
+  const removeKey = (obj, key) => {
+	let newObj = { ...obj }
+	delete newObj[key];
+	return newObj;
+  }
+
+  const combine = (obj1, obj2) => {
+	return { ...obj1, ...obj2 };
+  }
+
+
+  const update = (obj, key, val) => {
+	let newObj = { ...obj }
+	newObj[key] = val;
+	return newObj;
+  }
